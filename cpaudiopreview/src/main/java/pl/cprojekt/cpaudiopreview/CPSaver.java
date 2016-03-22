87f22bb0c6f472ba5997d51563cb6f18899423d5
biszkopta <cisiek@o2.pl>
2016-03-22 14:28:24 +0100

    maintenance code

--- cpaudiopreview/src/main/java/pl/cprojekt/cpaudiopreview/CPSaver.java
+++ cpaudiopreview/src/main/java/pl/cprojekt/cpaudiopreview/CPSaver.java
@@ -1,36 +1,25 @@
 package pl.cprojekt.cpaudiopreview;
 
-
-import android.util.Log;
-
 import java.lang.reflect.Field;
 
-//todo
+//todo not use
 public class CPSaver {
 
     public static void save(Class cl, Object object) {
-
-        Log.i("X", "nazwa klasy " + cl.getName());
-
         Field[] fields = cl.getDeclaredFields();
-
         for (Field field : fields) {
             if (!field.isAnnotationPresent(CPSaveState.class))
                 continue;
 
             try {
                 Class t = field.getType();
-                Log.i("X", "typ pola " + t.getName());
+                //Log.i("X", "typ pola " + t.getName());
                 field.get(object);
-                //bundle.putSerializable(field.getName(), field.get(object));
-                //bundle.put
+
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-
-
-            Log.d("X", "Pole: " + field.getName());
+            //Log.d("X", "Pole: " + field.getName());
         }
     }
-
 }
